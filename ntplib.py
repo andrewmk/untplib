853fa608406e0b2c51fdce7c743a5b877087ffff
Charles-Fran?ois Natali <cf.natali@gmail.com>
2014-02-02 16:01:05 +0100

    Update descriptions to match RFC 5905. Patch by Yann Ilas.

--- ntplib.py
+++ ntplib.py
@@ -45,27 +45,49 @@ class NTP:
     """delta between system and NTP time"""
 
     REF_ID_TABLE = {
-        'DNC': "DNC routing protocol",
-        'NIST': "NIST public modem",
-        'TSP': "TSP time protocol",
-        'DTS': "Digital Time Service",
-        'ATOM': "Atomic clock (calibrated)",
-        'VLF': "VLF radio (OMEGA, etc)",
-        'callsign': "Generic radio",
-        'LORC': "LORAN-C radionavidation",
-        'GOES': "GOES UHF environment satellite",
-        'GPS': "GPS UHF satellite positioning",
+        "GOES":  "Geostationary Orbit Environment Satellite",
+        "GPS\0": "Global Position System",
+        "GAL\0": "Galileo Positioning System",
+        "PPS\0": "Generic pulse-per-second",
+        "IRIG":  "Inter-Range Instrumentation Group",
+        "WWVB":  "LF Radio WWVB Ft. Collins, CO 60 kHz",
+        "DCF\0": "LF Radio DCF77 Mainflingen, DE 77.5 kHz",
+        "HBG\0": "LF Radio HBG Prangins, HB 75 kHz",
+        "MSF\0": "LF Radio MSF Anthorn, UK 60 kHz",
+        "JJY\0": "LF Radio JJY Fukushima, JP 40 kHz, Saga, JP 60 kHz",
+        "LORC":  "MF Radio LORAN C station, 100 kHz",
+        "TDF\0": "MF Radio Allouis, FR 162 kHz",
+        "CHU\0": "HF Radio CHU Ottawa, Ontario",
+        "WWV\0": "HF Radio WWV Ft. Collins, CO",
+        "WWVH":  "HF Radio WWVH Kauai, HI",
+        "NIST":  "NIST telephone modem",
+        "ACTS":  "NIST telephone modem",
+        "USNO":  "USNO telephone modem",
+        "PTB\0": "European telephone modem",
+        "LOCL":  "uncalibrated local clock",
+        "CESM":  "calibrated Cesium clock",
+        "RBDM":  "calibrated Rubidium clock",
+        "OMEG":  "OMEGA radionavigation system",
+        "DCN\0": "DCN routing protocol",
+        "TSP\0": "TSP time protocol",
+        "DTS\0": "Digital Time Service",
+        "ATOM":  "Atomic clock (calibrated)",
+        "VLF\0": "VLF radio (OMEGA,, etc.)",
+        "1PPS":  "External 1 PPS input",
+        "FREE":  "(Internal clock)",
+        "INIT":  "(Initialization)",
+        "\0\0\0\0":   "NULL",
     }
     """reference identifier table"""
 
     STRATUM_TABLE = {
-        0: "unspecified",
-        1: "primary reference",
+        0: "unspecified or invalid",
+        1: "primary reference (%s)",
     }
     """stratum table"""
 
     MODE_TABLE = {
-        0: "unspecified",
+        0: "reserved",
         1: "symmetric active",
         2: "symmetric passive",
         3: "client",
@@ -78,9 +100,9 @@ class NTP:
 
     LEAP_TABLE = {
         0: "no warning",
-        1: "last minute has 61 seconds",
-        2: "last minute has 59 seconds",
-        3: "alarm condition (clock not synchronized)",
+        1: "last minute of the day has 61 seconds",
+        2: "last minute of the day has 59 seconds",
+        3: "unknown (clock unsynchronized)",
     }
     """leap indicator table"""
 
@@ -410,11 +432,13 @@ def stratum_to_text(stratum):
     NTPException -- in case of invalid stratum
     """
     if stratum in NTP.STRATUM_TABLE:
-        return NTP.STRATUM_TABLE[stratum]
-    elif 1 < stratum < 255:
-        return "secondary reference (NTP)"
+        return NTP.STRATUM_TABLE[stratum] % (stratum)
+    elif 1 < stratum < 16:
+        return "secondary reference (%s)" % (stratum)
+    elif stratum == 16:
+        return "unsynchronized (%s)" % (stratum)
     else:
-        raise NTPException("Invalid stratum.")
+        raise NTPException("Invalid stratum or reserved.")
 
 
 def ref_id_to_text(ref_id, stratum=2):
@@ -439,7 +463,7 @@ def ref_id_to_text(ref_id, stratum=2):
         if text in NTP.REF_ID_TABLE:
             return NTP.REF_ID_TABLE[text]
         else:
-            return text
+            return "Unidentified reference source '%s'" % (text)
     elif 2 <= stratum < 255:
         return '%d.%d.%d.%d' % fields
     else:
