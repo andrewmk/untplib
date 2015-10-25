75a3f68179fe51e1f29da407dee1fed3aad8e002
andrewmk <andrew@hedges.me.uk>
2015-10-25 08:38:30 +0000

    Create untplib-client.py
    Test code for untplib

---
+++ untplib-client.py
@@ -0,0 +1,12 @@
+import untplib
+
+c=untplib.NTPClient()
+resp=c.request('0.uk.pool.ntp.org', version=3, port=123)
+print("Offset is ", resp.offset)
+
+from machine import RTC
+import time
+
+rtc = RTC()
+print("Adjusting clock by ", resp.offset, "seconds")
+rtc.init(time.localtime(time.time() + resp.offset))
